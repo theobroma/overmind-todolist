c6c65ec800c7b1ab4171b177f818ac5a2693c142
Alexandr <theobroma222@gmail.com>
2020-06-19 07:17:19 +0300

    rules-of-hooks

--- client/src/#/#/Main.tsx
+++ client/src/#/#/Main.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-// import styled from 'styled-components/macro';
 import TodoApp from '../../@components/TodoApp';
 
-// const Thing = styled.div`
-//   color: red;
-// `;
-
-interface Props {
-  // activeTodoCount: number;
-  // completedCount: number;
-}
-
-const MainApp: React.FC<Props> = (props) => {
+const MainApp: React.FC = () => {
   return (
     <div className="App">
       <section className="todoapp">
